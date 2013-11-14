f8ad2adb6c6f64474f8d78fa8108c67cedce6f4e
Peter Sudmant <psudmant@gmail.com>
2013-11-14 08:46:43 -0800

    inital commit

--- do_make_DTS_windowed_ests.sh
+++ do_make_DTS_windowed_ests.sh
@@ -1,15 +1,23 @@
-#input_genomes_dir=/net/eichler/vol7/home/psudmant/EEE_Lab/1000G/analysis_files/input_genomes_moved/hg19/current_working_genomes/split_C_team
-input_genomes_dir=/net/eichler/vol7/home/psudmant/EEE_Lab/1000G/analysis_files/input_genomes_moved/hg19/current_working_genomes/split_Neanderthal_42x_Denisova_Loshbour_HGDP/
+input_genomes_dir=/net/eichler/vol7/home/psudmant/EEE_Lab/1000G/analysis_files/input_genomes_moved/hg19/current_working_genomes/split_C_team
+#input_genomes_dir=/net/eichler/vol7/home/psudmant/EEE_Lab/1000G/analysis_files/input_genomes_moved/hg19/current_working_genomes/split_Loschbour_ABTeams_Neanderthal_Denisova/
 contigs=/net/eichler/vol7/home/psudmant/genomes/contigs/hg19_contigs.txt
 mask_file=/net/eichler/vol7/home/psudmant/genomes/mask_tracks/HG19-noWM-pad36
-#mask_file=/net/eichler/vol7/home/psudmant/genomes/sunk_tracks/hg19/hg19_sunk_depth_mask.dts
+sunk_mask_file=/net/eichler/vol7/home/psudmant/genomes/sunk_tracks/hg19/hg19_sunk_depth_mask.dts
+
 ##outdir=~/ev19/projects/human_population_sequencing/nobackups/psudmant/wnd_output_DTS/hg19/
-#outdir=/net/eichler/vol19/projects/human_population_sequencing/nobackups/psudmant/wnd_output_DTS/hg19/C_team
-outdir=./
-#outdir=/net/eichler/vol19/projects/human_population_sequencing/nobackups/psudmant/wnd_output_DTS/hg19/Neanderthal_42x_Denisova_Loshbour_HGDP_start_end
-#outdir=/net/eichler/vol20/projects/human_population_sequencing/nobackups/psudmant/wnd_output_DTS/hg19/1000_genomes_PhaseI
-#wnd_pickle_dir=/net/eichler/vol7/home/psudmant/EEE_Lab/1000G/1000genomesScripts/windowed_analysis/DTS_window_analysis/windows/hg19_slide/
-wnd_pickle_dir=/net/eichler/vol7/home/psudmant/EEE_Lab/1000G/1000genomesScripts/windowed_analysis/DTS_window_analysis/windows/hg19/
+
+#outdir=~/ev19/projects/apes/nobackups/psudmant/ancient_genomes/Loschbour/analysis/DTS_500_slide
+#sunk_outdir=~/ev19/projects/apes/nobackups/psudmant/ancient_genomes/Loschbour/analysis/DTS_500_sunk_slide
+
+outdir=~/ev19/projects/human_population_sequencing/nobackups/psudmant/wnd_output_DTS/hg19/C_team/500_bp_slide
+sunk_outdir=~/ev19/projects/human_population_sequencing/nobackups/psudmant/wnd_output_DTS/hg19/C_team/500_bp_sunk_slide
+
+wnds_pickle=/net/eichler/vol7/home/psudmant/EEE_Lab/1000G/1000genomesScripts/windowed_analysis/DTS_window_analysis/windows/hg19_slide/500_bp_windows.pkl
+sunk_wnds_pickle=/net/eichler/vol7/home/psudmant/EEE_Lab/1000G/1000genomesScripts/windowed_analysis/DTS_window_analysis/windows/hg19_sunk_slide/500_bp_windows.sunks.pkl
+
+wnd_width=500
+
+#wnd_pickle_dir=/net/eichler/vol7/home/psudmant/EEE_Lab/1000G/1000genomesScripts/windowed_analysis/DTS_window_analysis/windows/hg19/
 #gc_track=~/genomes/GC_tracks/hg19-gc-w200.GC_content
 
 #######HG18!!
@@ -25,20 +33,15 @@ wnd_pickle_dir=/net/eichler/vol7/home/psudmant/EEE_Lab/1000G/1000genomesScripts/
 #wnd_pickle_dir=/net/eichler/vol7/home/psudmant/EEE_Lab/1000G/1000genomesScripts/windowed_analysis/DTS_window_analysis/windows/hg18/
 #wnd_pickle_dir=/net/eichler/vol7/home/psudmant/EEE_Lab/1000G/1000genomesScripts/windowed_analysis/DTS_window_analysis/windows/hg18_GC_masked
 
-
 ###OR< none will default to the original
 ###THE BELOW is just for SPECIAL GC libs
 #params=_filtered_libs_analysis.GC3v2-NOWM-pad36_v2/fit_params-CALKAN-NOWM-pad36-simple-dim0-ed-all.per_bac_params
 
-for p in `ls $wnd_pickle_dir/*.pkl | grep 500`  
+#for p in `ls $wnd_pickle_dir/*.pkl | grep 500`  
+#	wnd_pickle=$p
+#	wnd_contig_file=$p.contigs
+for i in `ls $input_genomes_dir`
 do
-	wnd_pickle=$p
-	wnd_contig_file=$p.contigs
-	wnd_width=`echo $wnd_pickle | awk -F "/" '{print $(NF)}' | awk -F _ '{print $1}'`
-	for i in `ls $input_genomes_dir`
-	do
-		echo python generate_windowed_cp_ests.py --contig_file $contigs --mask_file $mask_file --genome  $input_genomes_dir/$i --out_prefix $outdir/${wnd_width}_bp  --wnd_pickle $wnd_pickle --wnd_contig_file $wnd_contig_file --wnd_width $wnd_width
-        # --sunk_based
-		#--param_file $params
-	done
+    echo python generate_windowed_cp_ests.py --contig_file $contigs --mask_file $mask_file --genome  $input_genomes_dir/$i --out_prefix $outdir/${wnd_width}_bp  --wnd_pickle $wnds_pickle --wnd_contig_file $wnds_pickle.contigs --wnd_width $wnd_width
+    echo python generate_windowed_cp_ests.py --contig_file $contigs --mask_file $sunk_mask_file --genome  $input_genomes_dir/$i --out_prefix $sunk_outdir/${wnd_width}_bp  --wnd_pickle $sunk_wnds_pickle --wnd_contig_file $sunk_wnds_pickle.contigs --wnd_width $wnd_width --sunk_based --param_file _filtered_libs_analysis.GC3v2-NOWM-pad36/fit_params-CALKAN-NOWM-pad36-simple-dim1-ed-0.per_1kb_params
 done
--- do_make_wnd_gglob.sh
+++ do_make_wnd_gglob.sh
@@ -10,15 +10,14 @@ sunk_wnd_contigs=/net/eichler/vol7/home/psudmant/EEE_Lab/1000G/1000genomesScript
 in_dir=/net/eichler/vol22/projects/1000_genomes_phase_II_III/nobackups/DTS/
 sunk_in_dir=/net/eichler/vol22/projects/1000_genomes_phase_II_III/nobackups/sunk_DTS/
 genotype_loci=/net/eichler/vol7/home/psudmant/public_html/1000_genomes/PHASE_II_III/CNV_calling/callsets/20101027_all_calls_NO_DUPS.sorted.gz
-gglob_dir=/net/eichler/vol7/home/psudmant/public_html/1000_genomes/PHASE_II_III/gglob
+gglob_dir=/net/eichler/vol20/projects/human_diversity_sequencing/nobackups/1kg_PHASE_I_II_III/gglob
 
 ##setup
 #echo python ~/EEE_Lab/projects/common_code/ssf_DTS_caller/gglob.py  --contigs $wnd_contigs --in_DTS_dir $in_dir --gglob_dir $gglob_dir --wnd_size 500 --wnd_slide 0 --sunk_contigs $sunk_wnd_contigs --in_sunk_DTS_dir $sunk_in_dir --init  
 
 for chr in `cat $wnd_contigs | awk '{print $1}'`
 do
-    echo python ~/EEE_Lab/projects/common_code/ssf_DTS_caller/gglob.py  --contigs $wnd_contigs --in_DTS_dir $in_dir --gglob_dir $gglob_dir --wnd_size 500 --wnd_slide 0 --sunk_contigs $sunk_wnd_contigs --in_sunk_DTS_dir $sunk_in_dir --setup_chr $chr
+    echo which python';' source ~/.bash_profile ';' which python ';' python ~/EEE_Lab/projects/common_code/ssf_DTS_caller/gglob.py  --contigs $wnd_contigs --in_DTS_dir $in_dir --gglob_dir $gglob_dir --wnd_size 500 --wnd_slide 0 --sunk_contigs $sunk_wnd_contigs --in_sunk_DTS_dir $sunk_in_dir --setup_chr $chr
 done
-
 
 
